3c15df1f3968eb83c0466a6ddb25726da18dc9a3
Akashvenkatesh-347 <akashvenkatesh347@gmail.com>
2026-09-02 20:01:18 +0530

    Update FailureReason.java file

--- src/main/java/com/recoverai/payment/FailureReason.java
+++ src/main/java/com/recoverai/payment/FailureReason.java
@@ -2,11 +2,23 @@ package com.recoverai.payment;
 
 public enum FailureReason {
 
-    INSUFFICIENT_FUNDS,
-    NETWORK_ERROR,
-    TIMEOUT,
-    CARD_EXPIRED,
-    INVALID_CARD,
-    PAYMENT_METHOD_DECLINED,
-    UNKNOWN
+    INSUFFICIENT_FUNDS(true),
+    NETWORK_ERROR(true),
+    TIMEOUT(true),
+
+    CARD_EXPIRED(false),
+    INVALID_CARD(false),
+    PAYMENT_METHOD_DECLINED(false),
+
+    UNKNOWN(false);
+
+    private final boolean retryable;
+
+    FailureReason(boolean retryable) {
+        this.retryable = retryable;
+    }
+
+    public boolean isRetryable() {
+        return retryable;
+    }
 }
